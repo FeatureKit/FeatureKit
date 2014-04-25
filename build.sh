69943e47c0e05e7479aa6ff9b4713f4ae43aa867
Daniel Thorpe <dan@blindingskies.com>
2014-04-25 12:37:16 +0200

    [DTFD-2]: Adds scripts for building in CI.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exits bash immediately if any command fails
+set -e
+
+# Will output commands as the run
+set -x
+
+# Want to know what ENV varibles Buildbox sets during the build?
+env | grep BUILDBOX
+
+# Update the submodules
+git submodule update --init --recursive --remote
+
+./buildscripts/test.sh -s DNTFeatures

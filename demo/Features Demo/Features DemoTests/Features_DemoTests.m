329c9abd6f02f833cbd8b0b4167dca4878e4e7b5
Daniel Thorpe <danthorpe@me.com>
2014-06-18 19:06:29 +0100

    [DTFD-15]: Fixes the sqlite dependencies.
    Signed-off-by: Daniel Thorpe <danthorpe@me.com>

--- demo/Features Demo/Features DemoTests/Features_DemoTests.m
+++ demo/Features Demo/Features DemoTests/Features_DemoTests.m
@@ -14,21 +14,9 @@
 
 @implementation Features_DemoTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
 }
 
 @end

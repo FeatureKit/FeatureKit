a93a6f9bc703bf3fb1bad960854489127917cfd9
Daniel Thorpe <danthorpe@me.com>
2014-06-18 18:31:18 +0100

    [DTFD-15]: Adds a single basic test class.
    Signed-off-by: Daniel Thorpe <danthorpe@me.com>

---
+++ project/DNTFeaturesTests/DNTFeatureTests.m
@@ -0,0 +1,39 @@
+//
+//  DNTFeatureTests.m
+//  DNTFeatures
+//
+//  Created by Daniel Thorpe on 18/06/2014.
+//  Copyright (c) 2014 Daniel Thorpe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DNTFeatureTests : XCTestCase
+
+@end
+
+@implementation DNTFeatureTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

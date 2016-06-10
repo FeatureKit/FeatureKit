dc63633bd462c9342d33056b55ab3b3831610600
Daniel Thorpe <danthorpe@me.com>
2016-06-10 07:08:19 +0100

    [DTFD-23]: Adds project

---
+++ Tests/Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by Daniel Thorpe on 10/06/2016.
+//
+//
+
+import XCTest
+@testable import Features
+
+class TestSuiteRuns: XCTestCase {
+
+    func test__suite_runs() {
+        XCTAssertTrue(true)
+    }
+}

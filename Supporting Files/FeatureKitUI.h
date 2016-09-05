a572fbdfeac834fac8e4a71ba970ae3a3e54de00
Daniel Thorpe <danthorpe@me.com>
2016-09-05 23:28:26 +0100

    Adds FeatureKitUI - an iOS framework (#41)
    * [FEA-41]: Adds FeatureKitUI framework
    
    * [FEA-41]: Refactors CI pipeline
    
    * [FEA-41]: moves the carthage step
    
    * [FEA-41]: Shares the FeatureKitUI scheme
    
    * [FEA-41]: Adds stage to copy carthage frameworks

---
+++ Supporting Files/FeatureKitUI.h
@@ -0,0 +1,17 @@
+//
+//  FeatureKitUI
+//
+//  Copyright © 2016 FeatureKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FeatureKitUI.
+FOUNDATION_EXPORT double FeatureKitUIVersionNumber;
+
+//! Project version string for FeatureKitUI.
+FOUNDATION_EXPORT const unsigned char FeatureKitUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeatureKitUI/PublicHeader.h>
+
+
